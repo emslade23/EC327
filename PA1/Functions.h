1a25fca9344e1e6c9cd9665ad693c21ac79cf7e3
Elizabeth Slade <35977934+emslade23@users.noreply.github.com>
2018-02-23 14:32:01 -0500

    Add files via upload

---
+++ PA1/Functions.h
@@ -0,0 +1,18 @@
+#ifndef Functions_h
+#define Functions_h
+
+
+
+extern const int ENTRIES;
+void initialize;
+bool checkCode;
+void writeDataToFile(const char *);
+void readDataFromFile(const char *);
+double findNyanCatValue(double);
+int factorial(int);
+int fibonacci(int);
+double findSqrtValue(double);
+double naturalLog(double);
+int findNextOddValue(int);
+
+#endif
